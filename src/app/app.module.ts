53294ebe0b4c99133124ede162d5e48da5fc2ce7
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-11-06 20:04:14 -0300

    Agrego componentes catalogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { CatalogoItemComponent } from './components/catalogo-item/catalogo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CatalogoComponent,
+    CatalogoItemComponent
   ],
   imports: [
     BrowserModule,
